806a76affd92ddf2f73a00de68b5d6593c630c0b
Gui <6315083+guillaC@users.noreply.github.com>
2026-03-23 22:10:11 +0100

    wp

--- DeskUI.DemoRCL/Desktop.razor.cs
+++ DeskUI.DemoRCL/Desktop.razor.cs
@@ -4,8 +4,8 @@ namespace DeskUI.DemoRCL
 {
     public partial class Desktop
     {
-        private Task OpenFirstWindow() => WindowManager.OpenWindowAsync<FirstForm>("FirstComponent", 240, 335);
-        private Task OpenSecondWindow() => WindowManager.OpenWindowAsync<SecondForm>("SecondComponent (Modal)", 550, 250, allowClose: false, overlayed: true);
+        private Task OpenFirstWindow() => WindowManager.OpenWindowAsync<FirstForm>("Sample Window", 240, 335);
+        private Task OpenSecondWindow(bool isOverLayed = false) => WindowManager.OpenWindowAsync<SecondForm>("Sample Window 2", 550, 350, overlayed: isOverLayed);
         private Task OpenUIManagerWindow() => WindowManager.OpenWindowAsync<UIManagerForm>("UIManager", 1200, 400, singleInstance: true);
         // Task OpenUIThemeBuilderWindow() => WindowManager.OpenWindowAsync<ThemeBuilder>("ThemeBuilder", 850, 400);
     }
