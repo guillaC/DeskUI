4bb904eca9405292d8951a665646c43d1c3f1162
Gui <6315083+guillaC@users.noreply.github.com>
2026-04-04 16:15:10 +0200

    trackPointerMove drag js smartphone

--- DeskUI/wwwroot/drag.js
+++ DeskUI/wwwroot/drag.js
@@ -1,5 +1,16 @@
-window.trackPointerMove = function (dotnetRef) {
-    window.addEventListener('pointermove', e => {
-        dotnetRef.invokeMethodAsync('HandleMouseMoveAsync', e.clientX, e.clientY);
-    });
+window.trackPointerMove = (dotnet) => {
+    let dragging = false;
+
+    window.onpointerdown = e => {
+        dragging = true;
+    };
+
+    window.onpointermove = e => {
+        if (dragging)
+            dotnet.invokeMethodAsync('HandleMouseMoveAsync', e.clientX, e.clientY);
+    };
+
+    window.onpointerup = e => {
+        dragging = false;
+    };
 };
